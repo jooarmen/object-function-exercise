dd55bbcc479bca489f2f2ff01b2ff48fe0b73357
jooarmen <126363143+jooarmen@users.noreply.github.com>
2023-03-31 12:49:13 -0700

    finished exercise

--- challenges.js
+++ challenges.js
@@ -1,21 +1,36 @@
 // Create a variable called myName and assign it your name as a string.
+let myName = "Jonathan";
 
 // Create an array called numbers and add five numbers to it.
+let numbers = [1, 2, 3, 4, 5];
 
 // Create a variable called myAge and assign it your age as a number.
+let myAge = "19";
 
 // Create an object called person with properties name, age, and gender.
+let person = {
+    name: "Jonathan",
+    age: 19,
+    gender: "Male",
+}
 
 // Create a variable called isRaining and assign it a boolean value based on whether it is currently raining or not.
+let isRaining = "true";
 
 // Create a function called greet that takes a name as a parameter and returns a greeting message using template literals.
+function greet(name){
+    return 'Hello ${name}';
+}
 
 // Create a function called multiply that takes two numbers as parameters and returns their product.
+function multiply(num1, num2){
+    return num1 * num2;
+}
 
 // Create a function called divide that takes two numbers as parameters and returns their quotient.
-
+function divide(num1, num2){
+    return num1/num2;
+}
 // BONUS
 // Create a function called isEven that takes a number as a parameter and returns true if the number is even, false otherwise.
-
-
-
+  
